cd20f3087219f4665831b6ce5d8e4966b2df5060
ivengeance57 <166838486+ivengeance57@users.noreply.github.com>
2024-04-14 18:29:13 +0000

    added an api

--- app.py
+++ app.py
@@ -25,12 +25,20 @@ def show_job(id):
   return render_template('jobpage.html', job=job)
 
 
+@app.route("/api/job/<id>")
+def show_json_job(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
   data = request.form
   job = load_job_from_db(id)
   add_application_to_db(id, data)
-  return render_template('application_submitted.html', application=data, job=job)
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
 
 
 if __name__ == "__main__":
