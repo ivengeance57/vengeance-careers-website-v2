9b018c19523ba41884ded51ef938b5cdb7db08b9
ivengeance57 <166838486+ivengeance57@users.noreply.github.com>
2024-04-14 18:20:13 +0000

    store application in db

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, json, jsonify, render_template
+from flask import Flask, json, jsonify, render_template, request
 
-from database import load_jobs_from_db, load_job_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -23,6 +23,14 @@ def show_job(id):
   if not job:
     return "Not Found", 404
   return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html', application=data, job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -16,9 +16,28 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs where id = :val"),  {"val": id})
+    result = conn.execute(text("select * from jobs where id = :val"),
+                          {"val": id})
     rows = result.all()
     if len(rows) == 0:
       return None
     else:
       return rows[0]._asdict()
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text(
+        "INSERT into applications_k(job_id, full_name, email, linkedin_url, education, work_experience, resume_url) values(:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+    conn.execute(
+        query, {
+            "job_id": job_id,
+            "full_name": data['full_name'],
+            "email": data['email'],
+            "linkedin_url": data['linkedin_url'],
+            "education": data['education'],
+            "work_experience": data['work_experience'],
+            "resume_url": data['resume_url']
+        })
+    conn.commit()
